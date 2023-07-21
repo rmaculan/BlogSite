b2a9f088754e9e406cb9be338c50aa42680db1a6
rmaculan <120978811+rmaculan@users.noreply.github.com>
2023-07-21 12:38:55 -0400

    Chat

---
+++ chat-server/index.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// Define your GraphQL schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Define your resolvers
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, world!',
+  },
+};
+
+// Create an instance of ApolloServer
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Start the server
+server.listen().then(({ url }) => {
+  console.log(`GraphQL server running at ${url}`);
+});
